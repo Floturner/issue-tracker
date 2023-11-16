bfa56646ff23ecf466513ca9dee1f64bee4f08fc
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-11-16 20:00:39 +0000

    Build an API for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,22 @@
+import prisma from '@/prisma/client';
+import { z } from 'zod';
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return Response.json(validation.error.errors, { status: 400 });
+  }
+
+  const issue = prisma.issue.create({
+    data: { ...validation.data },
+  });
+
+  return Response.json(issue, { status: 201 });
+}
