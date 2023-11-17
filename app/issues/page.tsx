35f9937ec05a19503d9a3e109176e1591595c7aa
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-11-17 11:03:22 +0000

    Show the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,52 @@
-import { Button } from '@radix-ui/themes';
+import prisma from '@/prisma/client';
+import { Button, Table, Text } from '@radix-ui/themes';
 import Link from 'next/link';
 
-export default function IssuesPage() {
+export default async function IssuesPage() {
+  const issues = await prisma.issue.findMany();
   return (
     <div>
-      <Button>
-        <Link href='/issues/new'>New Issue</Link>
-      </Button>
+      <div className='mb-5'>
+        <Button>
+          <Link href='/issues/new'>New Issue</Link>
+        </Button>
+      </div>
+      <Table.Root variant='surface'>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {!issues.length && (
+            <Table.Row>
+              <Table.Cell className='col-span-1 md:col-span-3'>
+                <Text>No issues found.</Text>
+              </Table.Cell>
+            </Table.Row>
+          )}
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className='block md:hidden'>{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 }
