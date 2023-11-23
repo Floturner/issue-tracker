c53233c2a3bff2a44951f17c60fcea0f5f779af4
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-11-23 11:29:57 +0000

    Change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Container } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -17,36 +17,40 @@ export default function NavBar() {
   ];
 
   return (
-    <Container className=' border-b mb-5 p-5'>
-      <nav className='flex space-x-6 items-center'>
-        <Link href='/'>
-          <AiFillBug />
-        </Link>
-        <ul className='flex space-x-6'>
-          {links.map((link) => (
-            <li key={link.href}>
-              <Link
-                className={classNames({
-                  'text-zinc-900': link.href === path,
-                  'text-zinc-500 hover:text-zinc-800': link.href !== path,
-                  'transition-colors': true,
-                })}
-                href={link.href}
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <Box>
-          {status === 'authenticated' && (
-            <Link href='/api/auth/signout'>Log out</Link>
-          )}
-          {status === 'unauthenticated' && (
-            <Link href='/api/auth/signin'>Login</Link>
-          )}
-        </Box>
-      </nav>
-    </Container>
+    <nav>
+      <Container mb='5' className='border-b'>
+        <Flex gap='5' p='5' align='center' justify='between'>
+          <Flex gap='5' align='center'>
+            <Link href='/'>
+              <AiFillBug />
+            </Link>
+            <ul className='flex space-x-6'>
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classNames({
+                      'text-zinc-900': link.href === path,
+                      'text-zinc-500 hover:text-zinc-800': link.href !== path,
+                      'transition-colors': true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === 'authenticated' && (
+              <Link href='/api/auth/signout'>Log out</Link>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href='/api/auth/signin'>Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
+    </nav>
   );
 }
