298286efba3a3fb72d4898ab8a562fbf0a94f37e
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-11-23 12:34:45 +0000

    Add loading skeleton to the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Skeleton } from '@/app/components';
 import {
   Avatar,
   Box,
@@ -62,7 +63,7 @@ function NavLinks() {
 function AuthStatus() {
   const { status, data: session } = useSession();
 
-  if (status === 'loading') return null;
+  if (status === 'loading') return <Skeleton width='3rem' />;
 
   if (status === 'unauthenticated') {
     return (
