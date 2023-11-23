db5c1d0769f705c692eef00a0494fa74fadc9430
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-11-23 20:15:22 +0000

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,25 +1,35 @@
 import prisma from '@/prisma/client';
-import { Status } from '@prisma/client';
+import { Issue, Status } from '@prisma/client';
+import { ArrowDownIcon, ArrowUpIcon } from '@radix-ui/react-icons';
 import { Box, Button, Flex, Table, Text } from '@radix-ui/themes';
-import delay from 'delay';
 import NextLink from 'next/link';
 import { IssueStatusBadge, Link } from '../../components';
 import IssueStatusFilter from './IssueStatusFilter';
 
+const columns: { label: string; value: keyof Issue; className?: string }[] = [
+  { label: 'Issue', value: 'title' },
+  { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
+  { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' },
+];
+
+const orders = ['asc', 'desc'] as const;
+type Order = (typeof orders)[number];
+type OrderBy = { [Property in keyof Issue]?: Order };
 type Props = {
-  searchParams: { status: Status };
+  searchParams: {
+    status: Status;
+    orderBy: keyof Issue;
+    order?: Order;
+  };
 };
 
 export default async function IssuesPage({ searchParams }: Props) {
-  const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
+  const { status, orderBy } = validateQueryParams(searchParams);
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
-
-  await delay(5000);
 
   return (
     <Box>
@@ -37,13 +47,27 @@ export default async function IssuesPage({ searchParams }: Props) {
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((col) => (
+              <Table.ColumnHeaderCell key={col.value} className={col.className}>
+                <NextLink
+                  href={{
+                    query: {
+                      ...searchParams,
+                      orderBy: col.value,
+                      order: searchParams.order === 'desc' ? 'asc' : 'desc',
+                    } as Props['searchParams'],
+                  }}
+                >
+                  {col.value === searchParams.orderBy &&
+                    (searchParams.order !== 'desc' ? (
+                      <ArrowUpIcon className='inline' />
+                    ) : (
+                      <ArrowDownIcon className='inline' />
+                    ))}
+                  {col.label}
+                </NextLink>
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -74,4 +98,22 @@ export default async function IssuesPage({ searchParams }: Props) {
       </Table.Root>
     </Box>
   );
+}
+
+function validateQueryParams(params: Props['searchParams']): {
+  status?: Status;
+  orderBy?: OrderBy;
+} {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(params.status) ? params.status : undefined;
+
+  const order: Order =
+    params.order && orders.includes(params.order) ? params.order : 'asc';
+
+  let orderBy: OrderBy | undefined = undefined;
+  if (columns.map((col) => col.value).includes(params.orderBy)) {
+    orderBy = { [params.orderBy]: order };
+  }
+
+  return { status, orderBy };
 }
